6f8452d0061a6f08e7a1029ff6dc2dbf2b3adf3b
fauzanalifnf <115539271+fauzanalifnf@users.noreply.github.com>
2022-10-18 18:52:28 +0700

    Add files via upload

---
+++ BelajarFungsi/src/Matematika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Matematika {
+    
+    int nilai1;
+    int nilai2;
+    
+    int tambah(){
+        int a = nilai1 + nilai2;
+        return a;
+ }
+    int kali(){
+        int hasil = nilai1* nilai2;
+        return hasil;
+    }
+    
+    int kurang(){
+        int hasil = nilai1-nilai2;
+        return hasil;
+    }
+    int bagi(){
+        int hasil = nilai1/nilai2;
+        return hasil;
+                
+    }
+}
---
+++ BelajarKonstanta/src/ContohKonstanta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class ContohKonstanta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Karyawan joko = new Karyawan();
+        joko.nama = "joko";
+        joko.Jabatan = "Programmer";
+        
+        System.out.println(joko.Perusahaan);
+    }
+    
+}
